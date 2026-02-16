142faf211dd72c8d2241f0d55d5fc7b5c0a80bb4
niemaminiona <f11w@o2.pl>
2026-02-16 20:24:12 +0100

    minor fixes, added comments

--- src/main/java/Test1/Launcher.java
+++ src/main/java/Test1/Launcher.java
@@ -2,11 +2,15 @@ package Test1;
 
 import org.lwjgl.glfw.GLFW;
 import org.lwjgl.glfw.GLFWErrorCallback;
+import org.lwjgl.glfw.GLFWVidMode;
 import org.lwjgl.opengl.GL11;
 
 public class Launcher implements Runnable {
     @Override
     public void run() {
+        int windowWidth = 640;
+        int windowHeight = 480;
+
         // Sets an error callback so GLFW errors are printed to stderr.
         GLFWErrorCallback.createPrint(System.err).set();
 
@@ -22,16 +26,24 @@ public class Launcher implements Runnable {
         // Requests the *core profile*
         GLFW.glfwWindowHint(GLFW.GLFW_OPENGL_PROFILE, GLFW.GLFW_OPENGL_CORE_PROFILE);
 
-        long window = GLFW.glfwCreateWindow(640, 480, "Render Engine", 0, 0);
+        long window = GLFW.glfwCreateWindow(windowWidth, windowHeight, "Render Engine", 0, 0);
 
         // Checks if the window creation failed (0 = NULL pointer).
         if(window == 0L){
             throw new IllegalStateException("Failed to create GLFW window");
         }
 
+        // Gets information about screen
+        GLFWVidMode vidMode = GLFW.glfwGetVideoMode(GLFW.glfwGetPrimaryMonitor());
+
+        // sets window to middle of the screen
+        GLFW.glfwSetWindowPos(window, (vidMode.width() - windowWidth) / 2, (vidMode.height() - windowHeight) / 2);
+
+        // Makes the OpenGL context of this window current on the calling thread so OpenGL commands affect this window
         GLFW.glfwMakeContextCurrent(window);
+
         // show the window
-//        GLFW.glfwShowWindow(window);
+        GLFW.glfwShowWindow(window);
 
         // sets key callback, when ESC is pressed, window closes
         GLFW.glfwSetKeyCallback(window, (win, key, scancode, action, mods) -> {
@@ -42,8 +54,11 @@ public class Launcher implements Runnable {
 
         // updates window
         while(!GLFW.glfwWindowShouldClose(window)){
-           update(window);
-           GLFW.glfwPollEvents();
+            // Calls an update function (as its name suggests it updates what is displayed on the window )
+            update(window);
+
+            // Checks for user input and window events (like key presses or close requests) and processes them
+            GLFW.glfwPollEvents();
         }
 
         // "Destroys" window and frees resources
@@ -62,6 +77,6 @@ public class Launcher implements Runnable {
         int width = w[0];
         int height = h[0];
 
-        GL11.glViewport(0, 0, width, height);
+//        GL11.glViewport(0, 0, width, height);
     }
 }
