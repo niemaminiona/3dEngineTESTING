5b6ca4ad72b4bd9c5471ceea982e7d115711e9a4
FilipWojcik4TP <f11w@o2.pl>
2026-02-16 14:28:18 +0100

    changed structure to launcher and main classes, added first implementation of launcher class

--- src/main/java/Test1/Launcher.java
+++ src/main/java/Test1/Launcher.java
@@ -1,9 +1,24 @@
 package Test1;
 
 import org.lwjgl.Version;
+import org.lwjgl.glfw.GLFW;
+import org.lwjgl.glfw.GLFWErrorCallback;
 
-public class Launcher {
-    public static void main(String[] args) {
-        System.out.println(Version.getVersion());
+public class Launcher implements Runnable {
+    @Override
+    public void run() {
+        GLFWErrorCallback.createPrint(System.err).set();
+
+        if (!GLFW.glfwInit()){
+            throw new IllegalStateException("Failed to initialize GLFW");
+        }
+
+        long window = GLFW.glfwCreateWindow(640, 480, "Render Engine", 0, 0);
+
+        if(window == 0L){
+            throw new IllegalStateException("Failed to create GLFW window");
+        }
+
+        GLFW.glfwTerminate();
     }
 }
---
+++ src/main/java/Test1/Main.java
@@ -0,0 +1,7 @@
+package Test1;
+
+public class Main {
+    public static void main(String[] args) {
+        new Thread(new Launcher()).start();
+    }
+}
